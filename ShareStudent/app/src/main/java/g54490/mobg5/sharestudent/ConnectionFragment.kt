b841bf92fe729944450a1af963981f42bd4b32e7
vivace fouefack <54490@etu.he2b.be>
2023-03-15 18:21:31 +0100

    intégration du fragment de connection dans l'interface principale

--- ShareStudent/app/src/main/java/g54490/mobg5/sharestudent/ConnectionFragment.kt
+++ ShareStudent/app/src/main/java/g54490/mobg5/sharestudent/ConnectionFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import g54490.mobg5.sharestudent.databinding.FragmentConnectionBinding
 
 class ConnectionFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connection, container, false)
+
+        val binding = DataBindingUtil.inflate<FragmentConnectionBinding>(inflater,
+            R.layout.fragment_connection,container,false)
+        return binding.root
     }
 }
