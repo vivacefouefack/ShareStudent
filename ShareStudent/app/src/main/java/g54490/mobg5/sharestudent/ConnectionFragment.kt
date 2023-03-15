8d58d24bd8510712bba401f854bf401e5bf71e70
vivace fouefack <54490@etu.he2b.be>
2023-03-15 19:30:42 +0100

    ajout du menu option

--- ShareStudent/app/src/main/java/g54490/mobg5/sharestudent/ConnectionFragment.kt
+++ ShareStudent/app/src/main/java/g54490/mobg5/sharestudent/ConnectionFragment.kt
@@ -1,11 +1,11 @@
 package g54490.mobg5.sharestudent
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import g54490.mobg5.sharestudent.databinding.FragmentConnectionBinding
 
 class ConnectionFragment : Fragment() {
@@ -16,6 +16,18 @@ class ConnectionFragment : Fragment() {
 
         val binding = DataBindingUtil.inflate<FragmentConnectionBinding>(inflater,
             R.layout.fragment_connection,container,false)
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
