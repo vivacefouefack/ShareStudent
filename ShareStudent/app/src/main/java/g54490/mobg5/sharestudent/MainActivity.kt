db595ab96774a1743e3ecdf3355bc41dcab382b1
vivace fouefack <54490@etu.he2b.be>
2023-03-16 23:18:41 +0100

    implémentation du tiroir

--- ShareStudent/app/src/main/java/g54490/mobg5/sharestudent/MainActivity.kt
+++ ShareStudent/app/src/main/java/g54490/mobg5/sharestudent/MainActivity.kt
@@ -2,19 +2,30 @@ package g54490.mobg5.sharestudent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import g54490.mobg5.sharestudent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //val navController = this.findNavController(R.id.myNavHostFragment)
-       // NavigationUI.setupActionBarWithNavController(this,navController)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
 }
