a2b65a2bbeb912ad60c9a362e35bf7cae5e9cbd6
vivace fouefack <54490@etu.he2b.be>
2023-04-15 20:57:07 +0200

    début de la remise 3

---
+++ ShareStudent/app/src/main/java/g54490/mobg5/sharestudent/connection/SplashScreen.kt
@@ -0,0 +1,12 @@
+package g54490.mobg5.sharestudent.connection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import g54490.mobg5.sharestudent.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
