996dd547afcab32d2030506720ced91fd5b1c334
vivace fouefack <54490@etu.he2b.be>
2023-05-26 19:16:29 +0200

    update profile fragment

--- ShareStudent/app/src/main/java/g54490/mobg5/sharestudent/viewmodel/ProfileViewModel.kt
+++ ShareStudent/app/src/main/java/g54490/mobg5/sharestudent/viewmodel/ProfileViewModel.kt
@@ -1,12 +1,36 @@
 package g54490.mobg5.sharestudent.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import g54490.mobg5.sharestudent.model.Publication
 import g54490.mobg5.sharestudent.model.Repository
 
 class ProfileViewModel: ViewModel(){
     private var _login = MutableLiveData<String>()
     var login: String=""
         get() = Repository.getUsername()
+
+    var nbPublication:Int=10//Repository.getMyPublications().size
+
+    private var _myPublication = MutableLiveData<List<Publication>>()
+    val myPublication: LiveData<List<Publication>>
+        get() = _myPublication
+
+    private val _navigateToPublicationDetail = MutableLiveData<String>()
+    val navigateToPublicationDetail
+        get() = _navigateToPublicationDetail
+
+    fun onPublicationClicked(id: String) {
+        _navigateToPublicationDetail.value = id
+    }
+
+    fun onPublicationDetailNavigated() {
+        _navigateToPublicationDetail.value = null
+    }
+
+    init {
+        _myPublication.value=Repository.getAllPublications()//Repository.getMyPublications()
+    }
 
 }
